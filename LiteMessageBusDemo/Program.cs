c3a09f2610d6fff76ecc9ef627c67529d35a1b64
linh.nguyen <lightalakanzam@gmail.com>
2019-10-18 14:29:34 +0700

    - Changed version to preview.

--- LiteMessageBusDemo/Program.cs
+++ LiteMessageBusDemo/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using LiteMessageBus.Services.Implementations;
 using LiteMessageBusDemo.Constants;
-using LiteMessageBusDemo.Models;
-using LiteMessageBusDemo.Services;
 
 namespace LiteMessageBusDemo
 {
@@ -17,43 +15,43 @@ namespace LiteMessageBusDemo
                     Console.WriteLine(
                         $"[IN-MEMORY] {DateTime.UtcNow:yyyy-MM-dd HH:mm:ss} - Message received: {message}"));
 
-            const string appId = "875585";
-            const string appKey = "7924946bbbd2fc014135";
-            const string appSecret = "a6c2c1f9ae29637d995a";
-            const string cluster = "ap1";
+            //const string appId = "875585";
+            //const string appKey = "7924946bbbd2fc014135";
+            //const string appSecret = "a6c2c1f9ae29637d995a";
+            //const string cluster = "ap1";
 
-            var pusherServerOptions = new PusherServer.PusherOptions();
-            pusherServerOptions.Cluster = cluster;
-            pusherServerOptions.Encrypted = true;
+            //var pusherServerOptions = new PusherServer.PusherOptions();
+            //pusherServerOptions.Cluster = cluster;
+            //pusherServerOptions.Encrypted = true;
 
-            var pusherClientOptions = new PusherClient.PusherOptions();
-            pusherClientOptions.Cluster = cluster;
-            pusherClientOptions.Encrypted = true;
+            //var pusherClientOptions = new PusherClient.PusherOptions();
+            //pusherClientOptions.Cluster = cluster;
+            //pusherClientOptions.Encrypted = true;
 
-            var broadcaster = new PusherServer.Pusher(appId,
-                appKey, appSecret, pusherServerOptions);
+            //var broadcaster = new PusherServer.Pusher(appId,
+            //    appKey, appSecret, pusherServerOptions);
 
-            var recipient = new PusherClient.Pusher(appKey, pusherClientOptions);
+            //var recipient = new PusherClient.Pusher(appKey, pusherClientOptions);
 
-            // Pusher message bus initialization.
-            var pusherMessageBus = new PusherLiteMessageBusService(broadcaster, recipient);
-            pusherMessageBus
-                .HookMessageChannel<Item>(MessageChannelConstants.Ui, MessageEventConstants.SendMessage)
-                .Subscribe(message =>
-                {
-                    Console.WriteLine(
-                        $"[PUSHER] {DateTime.UtcNow:yyyy-MM-dd HH:mm:ss} - Message received: {message}");
-                });
+            //// Pusher message bus initialization.
+            //var pusherMessageBus = new PusherLiteMessageBusService(broadcaster, recipient);
+            //pusherMessageBus
+            //    .HookMessageChannel<Item>(MessageChannelConstants.Ui, MessageEventConstants.SendMessage)
+            //    .Subscribe(message =>
+            //    {
+            //        Console.WriteLine(
+            //            $"[PUSHER] {DateTime.UtcNow:yyyy-MM-dd HH:mm:ss} - Message received: {message}");
+            //    });
 
-            recipient.ConnectAsync().Wait();
+            //recipient.ConnectAsync().Wait();
 
-            var item = new Item();
-            item.Name = "item-name-01";
-            item.Quantity = 10;
-            
+            //var item = new Item();
+            //item.Name = "item-name-01";
+            //item.Quantity = 10;
+
             inMemoryMessageBus.AddMessage(MessageChannelConstants.Ui, MessageEventConstants.SendMessage, "Hello world");
-            pusherMessageBus.AddMessage(MessageChannelConstants.Ui, MessageEventConstants.SendMessage, item);
-            Console.WriteLine("App started");
+            //pusherMessageBus.AddMessage(MessageChannelConstants.Ui, MessageEventConstants.SendMessage, item);
+            //Console.WriteLine("App started");
 
             Console.ReadLine();
         }
